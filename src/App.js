141e147ed66aa2ad17f2c773f9fa2c3d59661d48
lil-G <kahlil.rivera@yotpo.com>
2024-05-17 11:19:07 +0800

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   let InstanceId_SEO = "";
   let InstanceId_Carousel = ""; 
   let InstanceId_StarRating = "736982";
-  let InstanceId_PromotedProd = "";
+  let InstanceId_PromotedProd = "737009";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
