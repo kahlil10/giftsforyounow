2a61be52b8f170be4beef255178e4369c974335b
lil-G <kahlil.rivera@yotpo.com>
2024-05-17 12:52:28 +0800

    g

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "true";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
